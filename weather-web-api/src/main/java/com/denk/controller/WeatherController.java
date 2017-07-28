a2ad06a961b82ec139edfbf85ed0efd0c832c242
Dengke Lv(吕登科1) <lvdengke1@le.com>
2017-07-28 13:32:39 +0800

    add html

--- weather-web-api/src/main/java/com/denk/controller/WeatherController.java
+++ weather-web-api/src/main/java/com/denk/controller/WeatherController.java
@@ -1,14 +1,18 @@
 package com.denk.controller;
 
+import com.denk.model.City;
 import com.denk.model.Weather;
 import com.denk.service.CityService;
 import com.denk.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+@Controller
 public class WeatherController {
 
     @Autowired
@@ -18,14 +22,17 @@ public class WeatherController {
     private CityService cityService;
 
     @RequestMapping("/weather/{cityId}")
-    public String weather(@PathVariable String cityId) {
+    public String weather(Model model, @PathVariable String cityId) {
         Weather weather = weatherService.getWeatherByCityId(cityId);
-        return weather == null ? "no such city" : weather.toString();
+        model.addAttribute("weather", weather);
+        return "weather";
     }
 
     @RequestMapping("/city/list")
-    public String listCitys() {
-        return cityService.listCitys().toString();
+    public String listCitys(Model model) {
+        List<City> citys = cityService.listCitys();
+        model.addAttribute("citys", citys);
+        return "citys";
     }
 
 }
