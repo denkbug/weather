6a2923a9801aa23bc837b51b783c555112dd2c3e
Dengke Lv(吕登科1) <lvdengke1@le.com>
2017-09-06 09:48:06 +0800

    test stop watch

--- weather-web-api/src/test/java/com/denk/test/SimpleTest.java
+++ weather-web-api/src/test/java/com/denk/test/SimpleTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.StopWatch;
 
 
 /**
@@ -19,7 +20,26 @@ public class SimpleTest {
     private WeatherAPIController weatherApiController;
 
     @Test
-    public void testAPI(){
+    public void testAPI() {
         System.out.println(weatherApiController.listCitys());
+    }
+
+    public static void main(String[] args) throws Exception {
+        StopWatch sw = new StopWatch();
+        sw.start("读取文件");
+        Thread.sleep(1000);
+        sw.stop();
+        sw.start("文件删除");
+        Thread.sleep(100);
+        sw.stop();
+        sw.start("文件拷贝");
+        Thread.sleep(10);
+        sw.stop();
+        System.out.println(sw.prettyPrint());
+
+        long stime = System.currentTimeMillis();
+        Thread.sleep(1000);
+        long etime = System.currentTimeMillis();
+        System.out.println("执行时间:" + (etime - stime));
     }
 }
